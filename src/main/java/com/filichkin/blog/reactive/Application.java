d5047d99b351ef245779f0f14a6d19807e8a78d0
Michael Luo <michael.luo.sh@gmail.com>
2020-01-10 22:47:19 -0800

    Add multiple thread for data source. TODO: Add the streaming source like Kafka Consumer.

--- src/main/java/com/filichkin/blog/reactive/Application.java
+++ src/main/java/com/filichkin/blog/reactive/Application.java
@@ -1,5 +1,6 @@
 package com.filichkin.blog.reactive;
 
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
@@ -7,12 +8,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 @SpringBootApplication
 @Slf4j
@@ -26,15 +30,95 @@ public class Application {
 
     }
 
+    static class PriceTickerDataFeeder extends Thread {
+
+        private final Listener listener;
+
+        public PriceTickerDataFeeder(Listener listener) {
+            this.listener = listener;    
+        }
+
+        // Implement the backpressure from the data source
+        // Something like Kafka Consumer's Polling Thread
+        public void run() {
+            int count = 1;
+            while (true){
+                String e = "#" + (count++) +" @: " + System.currentTimeMillis();
+                log.info("1.GENERATE: " + e);
+                listener.onData(e);
+
+                try {
+                    sleep(100);
+                } catch (InterruptedException ex) {
+                    log.error("Thread interrupted:", ex);
+                    break;
+                }
+            }
+        }
+
+
+    }
+
+    interface Listener {
+        void onData(String data);
+    }
+
+    public static PriceTickerDataFeeder getPriceTickerDataFeeder() {
+        return null;
+    }
+
     public static void reactorCoreShowCase() {
+
+        Scheduler s = Schedulers.newParallel("parallel-scheduler", 4);
+
+        final Flux<String> flux1 = Flux
+                .range(1, 2)
+                .map(i -> 10 + i)
+                .publishOn(s)
+                .map(i -> "value " + i);
+
+        new Thread(() -> flux1.subscribe(e -> System.out.println("publisheOn() demo: " + Thread.currentThread().getName() + ":" + e))).start();
+
+        // Parallel stream
+        Flux.range(1, 10)
+                .parallel(2)
+                .subscribe(i -> System.out.println(Thread.currentThread().getName() + " -> " + i));
+
+        Flux.range(1, 10)
+                .parallel(2)
+                .runOn(Schedulers.parallel())
+                .subscribe(i -> System.out.println(Thread.currentThread().getName() + " -> " + i));
 
         // Hot Stream
         List<Integer> elements = new ArrayList<>();
 
-        ConnectableFlux<Object> publisher = Flux.create(fluxSink -> {
-            while(true) {
-                fluxSink.next(System.currentTimeMillis());
+        Flux<String> publisherFull = Flux.<String>create(new Consumer<FluxSink<String>>() {
+            @Override
+            public void accept(FluxSink<String> fluxSink) {
+
             }
+        });
+
+        ConnectableFlux<String> publisher = Flux.<String>create(fluxSink -> {
+
+            Listener dataListener = new Listener() {
+
+                int count = 0;
+                @Override
+                public void onData(String data) {
+                    fluxSink.next(data);
+
+                    count++;
+
+                    if (count> 7) {
+                        fluxSink.complete();
+                    }
+                }
+            };
+
+            PriceTickerDataFeeder feeder = new PriceTickerDataFeeder(dataListener);
+
+            feeder.start();
         })
                 .sample(Duration.ofMillis(100))
                 .log()
@@ -43,7 +127,7 @@ public class Application {
 
 //        publisher.subscribeOn(Schedulers.parallel());
         // publisher.subscribeOn(Schedulers.newSingle("thread-e1"));
-        publisher.subscribeOn(Schedulers.newParallel("pool", 5)).subscribe(o -> log.info(String.format("#1: %s", o)));
+        publisher.subscribeOn(Schedulers.newParallel("pool", 5)).subscribe(o -> log.info(String.format("2.SUBSCRIBE: %s", o)));
 //        publisher.subscribe(o -> log.info(String.format("#2: %s", o)));
 
         // This is blocking, should be added to another thread
